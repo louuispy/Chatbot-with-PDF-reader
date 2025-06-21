d2d7d37428d1f1c05e9f48020b049ca8ad59cd9b
Luis <luishenriquesousay.t.2018@gmail.com>
2025-06-21 15:27:10 -0300

    organizing features in their own files

--- app.py
+++ app.py
@@ -1,37 +1,26 @@
 import warnings
 import logging
 import streamlit as st
+from backend.rag_chatbot import create_vectorstore, create_conversation_chain
 
 def main():
 
-    # Tirando avisos e erros desnecessários
     warnings.filterwarnings('ignore')
     logging.getLogger('transformers').setLevel(logging.ERROR)
 
-    # Definindo o nome da página e o ícone
     st.set_page_config(page_title='AssistantAI', page_icon='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/512px-ChatGPT-Logo.svg.png?20240214002031')
 
     st.title('AssistantAI')
+    st.subheader('Como posso ajudar?')
 
-    # Verificando se já existem mensagens na sessão
-    # Se não, inicializando a lista de mensagens
     if 'messages' not in st.session_state:
-        st.session_state.messages = [] # Essa lista armazenará todas as mensagens do chat
+        st.session_state.messages = []
+    if 'conversation' not in st.session_state:
+        st.session_state.conversation = None    
     for message in st.session_state.messages:
         st.chat_message(message['role']).markdown(message['content'])
 
-    # O código acima é responsável por redesenhar o histórico com mensagens anteriores, ao recarregar a página 
-
-    st.subheader('Como posso ajudar?')
     prompt = st.chat_input('Pergunte alguma coisa sobre os PDFs enviados...', key='chat_input')
-
-    if prompt:
-        st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role': 'user', 'content': prompt})
-
-        response = "Olá, sou o AssistantAI! Como posso ajudar você hoje?"
-        st.chat_message('assistant').markdown(response)
-        st.session_state.messages.append({'role': 'assistant', 'content': response})
     
     with st.sidebar:
         st.subheader('Seus arquivos')
@@ -46,11 +35,24 @@ def main():
         try:
             if button_process:
                 if pdf_docs:
+                    vectorstore = create_vectorstore(pdf_docs)
+                    st.session_state.conversation = create_conversation_chain(vectorstore)
                     st.success('Arquivos processados com sucesso!')
                 else:
                     st.info('Nenhum arquivo carregado. Por favor, carregue um arquivo PDF para começar.')
         except Exception as e:
-            st.info(f'Erro ao carregar o arquivo {pdf_docs}. Verifique se o arquivo é um PDF válido.')
+            st.info(f'Erro ao carregar o arquivo {e}')
+
+    if prompt:
+        st.chat_message('user').markdown(prompt)
+        st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+        if st.session_state.conversation is None:
+            st.info("Por favor, carregue e processe seus PDFs clicando em 'Processar PDFs' antes de perguntar.")
+        else:
+            response = st.session_state.conversation.invoke({'question': prompt})
+            st.chat_message('assistant').markdown(response['answer'])
+            st.session_state.messages.append({'role': 'assistant', 'content': response['answer']})
 
 
 if __name__ == "__main__":
