3b5cd3bfeadf8d883db94305ff466905f436b993
Luis <luishenriquesousay.t.2018@gmail.com>
2025-09-22 12:04:19 -0300

    create pdf reader script

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import warnings
 import logging
 import streamlit as st
-from backend.rag_chatbot import create_vectorstore, create_conversation_chain
+from utils import text
+#from rag_chatbot import create_vectorstore, create_conversation_chain
 
 def main():
 
@@ -13,14 +14,7 @@ def main():
     st.title('AssistantAI')
     st.subheader('Como posso ajudar?')
 
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
-    if 'conversation' not in st.session_state:
-        st.session_state.conversation = None    
-    for message in st.session_state.messages:
-        st.chat_message(message['role']).markdown(message['content'])
 
-    prompt = st.chat_input('Pergunte alguma coisa sobre os PDFs enviados...', key='chat_input')
     
     with st.sidebar:
         st.subheader('Insira sua chave de API Groq')
@@ -32,7 +26,6 @@ def main():
                 if GROQ_API_KEY:
                     st.info('Chave de API enviada!')
                     st.session_state.GROQ_API_KEY = GROQ_API_KEY
-                    
                 else:
                     st.warning('Nenhuma chave de API inserida! Por favor, insira uma chave de API.')
         except Exception as e:
@@ -51,26 +44,14 @@ def main():
             if button_process_pdf:
                 if pdf_docs:
                     st.info('Arquivos enviados!')
-                    vectorstore = create_vectorstore(pdf_docs)
+                    all_files_text = text.process_files(pdf_docs)
                     st.info('Aguarde mais um pouco, estamos processando os arquivos...')
-                    st.session_state.conversation = create_conversation_chain(vectorstore, st.session_state.GROQ_API_KEY)
                     st.success('Arquivos processados com sucesso!')
+                    print(all_files_text)
                 else:
                     st.info('Nenhum arquivo carregado. Por favor, carregue um arquivo PDF para começar.')
         except Exception as e:
             st.warning(f'Erro ao carregar o arquivo {e}')
-
-    if prompt:
-        st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role': 'user', 'content': prompt})
-
-        if st.session_state.conversation is None:
-            st.info("Por favor, carregue e processe seus PDFs clicando em 'Processar PDFs' antes de perguntar.")
-        else:
-            response = st.session_state.conversation.invoke({'question': prompt, "chat_history": st.session_state.messages})
-            st.chat_message('assistant').markdown(response['answer'])
-            st.session_state.messages.append({'role': 'assistant', 'content': response['answer']})
-
 
 if __name__ == "__main__":
 
