b94a548bef77e0ce97c40fb9a4c0d7ad95c5cabe
Luis <luishenriquesousay.t.2018@gmail.com>
2025-09-25 12:20:42 -0300

    create conversation chain without pdf

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import warnings
 import logging
-import os
 import streamlit as st
-from streamlit_chat import message
 from utils import process_embeddings, text, chatbot
-#from rag_chatbot import create_vectorstore, create_conversation_chain
 
 def main():
 
     warnings.filterwarnings('ignore')
     logging.getLogger('transformers').setLevel(logging.ERROR)
 
-    st.set_page_config(page_title='AssistantAI', page_icon='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/512px-ChatGPT-Logo.svg.png?20240214002031')
+    st.set_page_config(
+        page_title='AssistantAI',
+        page_icon='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/512px-ChatGPT-Logo.svg.png?20240214002031',
+        layout='wide',
+        initial_sidebar_state='expanded'
+        )
 
     st.title('AssistantAI')
     st.subheader('Como posso ajudar?')
@@ -20,24 +22,28 @@ def main():
 
     if 'messages' not in st.session_state:
         st.session_state.messages = []
+
     if 'conversation_chain' not in st.session_state:
-        st.session_state.conversation_chain = None    
+        st.session_state.conversation_chain = None
+
+    if 'conversation_without_pdf' not in st.session_state:
+        st.session_state.conversation_without_pdf = None    
+
+    if 'mode' not in st.session_state:
+        st.session_state.mode = None
+
+    if 'pdf_docs' not in st.session_state:
+        st.session_state.pdf_docs = []
+
     for message in st.session_state.messages:
         st.chat_message(message['role']).markdown(message['content'])
-
-    if (user_question):
-        response = st.session_state.conversation_chain(user_question)
-
-        st.chat_message('user').markdown(user_question)
-        st.session_state.messages.append({'role': 'user', 'content': user_question})
-
-        st.chat_message('assistant').markdown(response['answer'])
-        st.session_state.messages.append({'role': 'assistant', 'content': response['answer']})
 
     with st.sidebar:
         st.subheader('Insira sua chave de API Groq')
         input_api_key = st.text_input('Insira sua chave de API Groq', key='groq_api_key', type='password')
         button_process_api = st.button('Enviar chave de API')
+
+        st.markdown('---')
 
         try:
             if button_process_api:
@@ -50,31 +56,74 @@ def main():
         except Exception as e:
             st.warning(f'Erro ao enviar a chave de API {e}')
 
-        st.subheader('Seus arquivos')
-        pdf_docs = st.file_uploader(
-            label='Carregue seus arquivos PDF',
-            type='pdf',
-            accept_multiple_files=True
-        )
 
-        button_process_pdf = st.button('Processar PDFs')
+        st.subheader('Qual tarefa você deseja que o AssistantAI faça?')
+        button_pdf = st.button('Tirar dúvidas de PDFs')
+        button_conversation_without_pdf = st.button('Conversa livre')
 
-        try:
-            if button_process_pdf:
-                if pdf_docs:
-                    st.info('Arquivos enviados!')
-                    all_files_text = text.process_files(pdf_docs)
+        st.markdown('---')
 
-                    st.info('Aguarde mais um pouco, estamos processando os arquivos...')
-                    chunks = text.create_text_chunks(all_files_text)
-                    vectorstore = process_embeddings.create_vectorstore(chunks)
-                    st.session_state.conversation_chain = chatbot.create_conversation_chain(vectorstore, st.session_state.api_key)
-                    
-                    st.success('Arquivos processados com sucesso!')
-                else:
-                    st.info('Nenhum arquivo carregado. Por favor, carregue um arquivo PDF para começar.')
-        except Exception as e:
-            st.warning(f'Erro ao carregar o arquivo {e}')
+        if (button_pdf):
+            st.session_state.mode = 'pdf'
+
+        if st.session_state.mode=='pdf':
+            st.subheader('Seus arquivos')
+            pdf_docs = st.file_uploader(
+                label='Carregue seus arquivos PDF',
+                type='pdf',
+                accept_multiple_files=True
+            )
+
+            button_process_pdf = st.button('Processar PDFs')
+
+            try:
+                if button_process_pdf:
+                    if pdf_docs:
+                        st.session_state.pdf_docs = pdf_docs
+                        st.info('Arquivos enviados!')
+                        all_files_text = text.process_files(st.session_state.pdf_docs)
+
+                        st.info('Aguarde mais um pouco, estamos processando os arquivos...')
+                        chunks = text.create_text_chunks(all_files_text)
+                        vectorstore = process_embeddings.create_vectorstore(chunks)
+                        st.session_state.conversation_chain = chatbot.create_conversation_chain(vectorstore, st.session_state.api_key)
+
+                        st.success('Arquivos processados com sucesso!')
+                    else:
+                        st.info('Nenhum arquivo carregado. Por favor, carregue um arquivo PDF para começar.')
+            except Exception as e:
+                st.warning(f'Erro ao carregar o arquivo {e}')
+
+        if (button_conversation_without_pdf):
+            st.session_state.mode = 'free'
+            st.session_state.conversation_without_pdf = chatbot.create_conversation_without_pdf(st.session_state.api_key)
+
+            st.subheader('Envie sua mensagem e converse com o Assistant!')
+    
+    if (user_question) and (st.session_state.mode=='pdf'):
+        st.chat_message('user').markdown(user_question)
+        st.session_state.messages.append({'role': 'user', 'content': user_question})
+        with st.spinner("Analisando sua pergunta..."):
+            try:
+                response = st.session_state.conversation_chain(user_question)
+
+            
+                st.chat_message('assistant').markdown(response['answer'])
+                st.session_state.messages.append({'role': 'assistant', 'content': response['answer']})
+            except Exception as e:
+                st.error(f'Erro: {e}')
+
+    elif (user_question) and (st.session_state.mode=='free'):
+        st.chat_message('user').markdown(user_question)
+        st.session_state.messages.append({'role': 'user', 'content': user_question})
+        with st.spinner("Analisando sua pergunta..."):
+            try:
+                conversation_with_no_pdf = st.session_state.conversation_without_pdf(user_question)
+                st.chat_message('assistant').markdown(conversation_with_no_pdf['response'])
+                st.session_state.messages.append({'role': 'assistant', 'content': conversation_with_no_pdf['response']})
+
+            except Exception as e:
+                st.error(f'Erro: {e}')
 
 if __name__ == "__main__":
 
--- utils/chatbot.py
+++ utils/chatbot.py
@@ -2,6 +2,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationChain
 
 def create_conversation_chain(vectorstore, key):
     llm = ChatGroq(
@@ -19,3 +20,23 @@ def create_conversation_chain(vectorstore, key):
     )
 
     return conversation_chain
+
+template = ChatPromptTemplate.from_messages([
+        ("system", """Você é um assistente de inteligência artificial simpático e profissional. *Seu nome é Assistant.* Você responde no idioma de prompt do usuário. Você sempre responde de forma clara, objetiva e precisa as dúvidas dos usuários. Você responde com base no contexto fornecido pelos usuários. Além disso, caso o usuário diga palavras, como: "Tchau", "Xau", "OK", "Okay", "Obrigado", ou palavras semelhantes, isso representa que ele deseja encerrar a conversa, portanto, você deve responder: De nada! Gostaria de mais alguma ajuda?"""),
+        ("user", "{history}, {input}")])
+
+def create_conversation_without_pdf(key):
+    llm = ChatGroq(
+        api_key=key,
+        temperature=1,
+        model="openai/gpt-oss-20b",
+    )
+
+    conversation_chain_without_pdf = ConversationChain(
+        llm=llm,
+        prompt=template,
+        verbose=True,
+        memory=ConversationBufferMemory()
+        )
+
+    return conversation_chain_without_pdf
